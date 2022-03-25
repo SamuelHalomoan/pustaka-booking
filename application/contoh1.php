d5bb0bd00504a716943d6923b5b756adc110cbc3
SamuelHalomoan <samhalomoan15@gmail.com>
2022-03-25 22:21:02 +0700

    pustaka booking

---
+++ application/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Desi Rona Uli lumban Raja
+        saya tinggal di daerah Bekasi olah raga yang saya sukai adalh Bulutangkis";
+    }
+}
